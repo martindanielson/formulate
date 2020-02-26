90c6e915e71dcc8846a2caac67e9e22426df19d6
Martin Danielson <martin@rocketblast.com>
2020-02-26 21:00:41 +0100

    Updates

---
+++ precache-manifest.39427483847a881c004ebc48b4dc82e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a0c6d456569039ae8081aa7e840d269",
+    "url": "/formulate/index.html"
+  },
+  {
+    "revision": "067cc3b063a5bffbd160",
+    "url": "/formulate/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "71f2bb6ac34ded029272",
+    "url": "/formulate/static/js/2.ffbe1044.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/formulate/static/js/2.ffbe1044.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "067cc3b063a5bffbd160",
+    "url": "/formulate/static/js/main.987bbfd3.chunk.js"
+  },
+  {
+    "revision": "c6b3cdf5fbd1486b16ce",
+    "url": "/formulate/static/js/runtime-main.94cbb770.js"
+  }
+]);
